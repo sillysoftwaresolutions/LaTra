1ca843ec3d2fb9da51e4d32432e3653cd3404219
Tresonic <37078118+Tresonic@users.noreply.github.com>
2019-07-07 21:09:54 +0200

    Add files via upload

--- LaserManchesterTransceiverLibrary/LaserManchesterTransceiverLibrary.ino
+++ LaserManchesterTransceiverLibrary/LaserManchesterTransceiverLibrary.ino
@@ -1,8 +1,8 @@
 #include "LaserTransceiver.h"
 
-LaserTransceiver transceiver(3, 200);
+LaserTransceiver transceiver(3, 20, A0);
 
-String msg = "hallo";
+String msg;
 
 void setup() {
   Serial.begin(115200);
@@ -10,10 +10,21 @@ void setup() {
 }
 
 void loop() {
-  //while(Serial.available() > 0)
-  transceiver.transmit(msg, msg.length());
-  while(!transceiver.getTransmitFinished()) {
-    transceiver.tickTransmitter();
+  bool toTransmit = 0;
+  
+  while(Serial.available() > 0) {
+    msg += Serial.readString();
+    toTransmit = 1;
+    Serial.println(msg);
+  }
+  
+  if(toTransmit) {
+    toTransmit = 0;
+    transceiver.transmit(msg, msg.length());
+    while(!transceiver.getTransmitFinished()) {
+      transceiver.tickTransmitter();
+    }
+    Serial.println("out");
   }
 
 }
--- LaserManchesterTransceiverLibrary/LaserTransceiver.h
+++ LaserManchesterTransceiverLibrary/LaserTransceiver.h
@@ -5,14 +5,21 @@
 
 class LaserTransceiver {
   public:
-    LaserTransceiver(byte pin, uint16_t baseLength);
+    LaserTransceiver(byte lPin, uint16_t baseLength, uint8_t ldPin);
     void transmit(String buf, uint8_t msgLength);
     bool getTransmitFinished();
     void tickTransmitter();
+    void tickReceiver();
+    void setReceiveThreshold(uint16_t val);
+    bool getReceiveByteAvailable();
+    String getReceive();
 
   private:
-    int laserPin;
-
+    bool bitDecode();
+  
+    uint8_t laserPin;
+    uint8_t ldrPin;
+    
     unsigned int halfClock;
     bool secondHalf = 0;
     char msg[16];
@@ -26,6 +33,15 @@ class LaserTransceiver {
     bool transmitFinished = 0;
     unsigned long halfStart = 0;
     uint16_t baseLength;
+
+    int analogThreshold = 0;
+    unsigned long lastEdge = 0;
+
+    bool oldBit = 0;
+    byte receiveByte = 0;
+    byte receiveByteIndex = 0;
+    bool receiveByteAvailable = 0;
+    unsigned int bitTime;
 
 };
 
